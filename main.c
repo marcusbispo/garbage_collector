5e8e62ef557553c50afdade84a2be99e8ab5d944
Marcus Vinicius Bispo Barbosa <marcusvbbarbosa@gmail.com>
2021-07-20 20:44:07 -0300

    Updating the main.c

--- main.c
+++ main.c
@@ -3,20 +3,21 @@
 #include <stdlib.h>
 
 int main(){
-  printf("1\n");
-    int *a = malloc2(sizeof(int));
+    printf("1\n");                                  // Each "printf("k\n");" as k being a number is only to help us visualize what is happening
+    int *a = malloc2(sizeof(int));                  // Okay... We allocated memory and the pointer a is pointing to the address of the memory allocated
+    dump();                                         // All dump()'s only print our heap
+
+    printf("2\n");
+    int *b = malloc2(sizeof(int));                  // We made the same for the pointer b
     dump();
-  printf("2\n");
-    int *b = malloc2(sizeof(int));
+
+    printf("3\n");
+    atrib2(a, b);                                   // Then we use the atrib2 function assign a to b
     dump();
-  printf("3\n");
-    atrib2(a, b);
+
+    printf("4\n");
+    atrib2(b, NULL);                                // Now we assign NULL to b
     dump();
-  printf("4\n");
-    atrib2(b, NULL);
-    dump();
-  printf("\nDigite um numero\n-> ");
-  scanf("%i", a);
-  printf("\nO numero digitado foi %i\n", *a);
+
     return 0;
 }
